5f9d7da4a79129805be1367273313536f8dce20a
Ndako <yharunandako@gmail.com>
2025-05-25 12:29:08 +0100

    update v1.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,50 @@
-import React, { useState } from "react";
-// import reactLogo from "./assets/react.svg";
-// import viteLogo from "/vite.svg";
-import Home from "./components/home";
+import React, { lazy, Suspense } from "react";
+import { Audio } from "react-loader-spinner";
+// import Home from "./components/home";
 import NavBar from "./components/navbar";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import BlogDetails from "./components/blogdetail";
-import CreateBlog from "./components/createblog";
-import EditBlog from "./components/edditBlog";
-import useFetch from "./usefetch";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import BlogDetails from "./components/blogdetail";
+// import CreateBlog from "./components/createblog";
+// import EditBlog from "./components/edditBlog";
+// import useFetch from "./usefetch";
 import Footer from "./components/footer";
-import Notfound from "./components/Notfound";
+//import Notfound from "./components/Notfound";
 function App() {
+  const Home = lazy(() => import("./components/home"));
+  const BlogDetails = lazy(() => import("./components/blogdetail"));
+  const CreateBlog = lazy(() => import("./components/createblog"));
+  const EditBlog = lazy(() => import("./components/edditBlog"));
+  const Notfound = lazy(() => import("./components/Notfound"));
   return (
-    <Router>
-      <NavBar />
-      <Routes>
-        <Route exact path="/EliteBlog/" element={<Home />} />
-        <Route path="/EliteBlog/create" element={<CreateBlog />} />
-        <Route path="/blogs/:id" element={<BlogDetails />} />
-        <Route path="/blogs/:id/edit" element={<EditBlog />} />
-        <Route path="*" element={<Notfound />} />
-      </Routes>
-      <Footer />
-    </Router>
+    <div className="App">
+      <BrowserRouter basename="/EliteBlog">
+        <Suspense
+          fallback={
+            <div className="loader">
+              <Audio
+                height="80"
+                width="80"
+                color="#4fa94d"
+                ariaLabel="audio-loading"
+                wrapperStyle={{}}
+                wrapperClass=""
+                visible={true}
+              />
+            </div>
+          }
+        >
+          <NavBar />
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/create" element={<CreateBlog />} />
+            <Route path="/blogs/:id" element={<BlogDetails />} />
+            <Route path="/blogs/:id/edit" element={<EditBlog />} />
+            <Route path="/*" element={<Notfound />} />
+          </Routes>
+          <Footer />
+        </Suspense>
+      </BrowserRouter>
+    </div>
   );
 }
 
